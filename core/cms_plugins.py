23fdedd6a44bec4bd6eb14894d480a6f926e8426
vashisthNitesh <119779637+vashisthNitesh@users.noreply.github.com>
2026-02-07 21:52:13 +0700

    Add team member list grid plugin

--- core/cms_plugins.py
+++ core/cms_plugins.py
@@ -2,7 +2,19 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from django.utils.translation import gettext_lazy as _
 
-from .models import TeamMemberDetailPlugin, TeamMemberListPlugin
+from .models import TeamMemberDetailPlugin, TeamMemberListGrid, TeamMemberListPlugin
+
+
+@plugin_pool.register_plugin
+class TeamMemberListGridCMSPlugin(CMSPluginBase):
+    model = TeamMemberListGrid
+    name = _('Team Member List Grid')
+    module = _('Content')
+    render_template = 'core/plugins/team_member_list_grid.html'
+    allow_children = True
+    child_classes = ['TeamMemberListPluginCMSPlugin']
+    cache = False
+    icon = 'cms-icon cms-icon-squares'
 
 
 @plugin_pool.register_plugin
